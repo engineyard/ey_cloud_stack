954c3024ff9877d67d1c7f0d77b05b6ae9d79bac
Larry Diehl <larrytheliquid@gmail.com>
2010-06-11 15:08:49 -0700

    some failz

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,4 @@
+raise 'failure will robinson'
 run "echo \"RAILS_ENV: $RAILS_ENV\" >> #{release_path}/before_symlink.log"
 run "echo \"shared_path: #{shared_path}\" >> #{release_path}/before_symlink.log"
 run "echo \"current_path: #{current_path}\" >> #{release_path}/before_symlink.log"
