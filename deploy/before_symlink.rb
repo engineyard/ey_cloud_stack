91b3e962e0e682c93df403dffb30a27853f668a6
Tim Carey-Smith <tim@spork.in>
2010-06-16 20:47:35 -0700

    Upgrade to rails 2.3.8 and bundler 0.9

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -2,3 +2,4 @@ run "echo \"RAILS_ENV: $RAILS_ENV\" >> #{release_path}/before_symlink.log"
 run "echo \"shared_path: #{shared_path}\" >> #{release_path}/before_symlink.log"
 run "echo \"current_path: #{current_path}\" >> #{release_path}/before_symlink.log"
 
+run "cd #{release_path} && /usr/bin/env HOME=/home/ey bundle install"
