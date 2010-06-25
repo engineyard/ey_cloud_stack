6ee137fff1de20f725121d71bc74598fd345165a
Larry Diehl <larrytheliquid@gmail.com>
2010-06-25 15:26:00 -0700

    Invalid Ruby syntax in before migrate hook

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,4 @@
+^^aoighe&&
 run "echo \"RAILS_ENV: $RAILS_ENV\" >> #{release_path}/before_migrate.log"
 run "echo \"shared_path: #{shared_path}\" >> #{release_path}/before_migrate.log"
 run "echo \"current_path: #{current_path}\" >> #{release_path}/before_migrate.log"
